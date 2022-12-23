8ea1d32f0c9f6cdb045706ee23aa60a7abe5b0c1
vlad <vlad19972017@gmail.com>
2022-12-24 01:45:42 +0200

    Basic project code.

---
+++ Credentials.Identity/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Credentials.Identity
+{
+    internal sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
